7bc9f91529a40cc5286abb6bbad8f1fe878b0bb6
Gowtham <gowthammetalion@gmail.com>
2026-06-30 15:27:52 -0500

    Implemented interactive chatbot loop

--- main.py
+++ main.py
@@ -7,24 +7,30 @@ print("Program started")
 load_dotenv()
 
 api_key = os.getenv("OPENROUTER_API_KEY")
-
 client = OpenAI(
     api_key=api_key,
     base_url="https://openrouter.ai/api/v1",
 )
+while True:
+    question=input("You: ")
+    if question=="exit":
+        break
+    else:
 
-response = client.chat.completions.create(
+        response = client.chat.completions.create(
     model="cohere/north-mini-code-20260617:free",
     max_tokens=300,
     messages=[
         {
             "role":"user",
-         "content": "Tell me an interesting fact about astrophysics."
+         "content": question
          }
     ]
 )
+        print(response.choices[0].message.content)
 
-print(response.choices[0].message.content)
+
+
 
 
 
