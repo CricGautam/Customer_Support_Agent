37170075f3f1204407b0fddca44c38d71889b8ff
Gowtham <gowthammetalion@gmail.com>
2026-06-30 09:57:08 -0500

    Build first AI Chatbot using Openrouter API

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
-print("Hello AI Enigineer")
+from openai import OpenAI
+from dotenv import load_dotenv
+import os
+
+print("Program started")
+
+load_dotenv()
+
+api_key = os.getenv("OPENROUTER_API_KEY")
+
+client = OpenAI(
+    api_key=api_key,
+    base_url="https://openrouter.ai/api/v1",
+)
+
+response = client.chat.completions.create(
+    model="cohere/north-mini-code-20260617:free",
+    max_tokens=300,
+    messages=[
+        {
+            "role":"user",
+         "content": "Tell me an interesting fact about astrophysics."
+         }
+    ]
+)
+
+print(response.choices[0].message.content)
+
+
+
+
+
